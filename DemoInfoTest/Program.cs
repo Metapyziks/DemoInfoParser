bdbeda0b44008267ed94bea390cd417bd5b3a5a9
James King <james.king@facepunchstudios.com>
2017-05-31 15:49:35 +0100

    Finally realised it was protobuf all along :unamused:

--- DemoInfoTest/Program.cs
+++ DemoInfoTest/Program.cs
@@ -1,350 +1,149 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
-using System.Security.Cryptography;
-using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading.Tasks;
+using System.Reflection;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+using ProtoBuf;
 
 namespace DemoInfoTest
 {
-    class RoundData
+    [ProtoContract]
+    public class DemoInfoMessage
     {
-        public TimeSpan? GameTime;
+        private static readonly DateTime Epoch = new DateTime( 1970, 1, 1, 0, 0, 0, DateTimeKind.Utc );
 
-        public struct TeamData
+        [ProtoMember( 1 ), JsonIgnore]
+        private ulong StartTimeSeconds64 { get; set; }
+
+        [ProtoMember( 2 ), JsonIgnore]
+        private uint StartTimeSeconds { get; set; }
+
+        public DateTime StartTime
         {
-            public int? Score;
-        }
-
-        public struct PlayerData
-        {
-            public SteamId? Player;
-
-            public int? Kills;
-            public int? Assists;
-            public int? Deaths;
-            public int? Score;
-            public int? EnemyKills;
-            public int? Headshots;
-            public int? Mvps;
-        }
-
-        public readonly TeamData[] Teams = new TeamData[2];
-        public readonly PlayerData[] Players = new PlayerData[10];
-    }
-
-    enum SteamIdFormat
-    {
-        V1,
-        V3
-    }
-
-    struct SteamId
-    {
-        private static readonly Regex _sOldFormatRegex = new Regex( @"^STEAM_(?<universe>[0-5]):(?<bit>[0-1]):(?<id>[0-9]+)$" );
-        private static readonly Regex _sNewFormatRegex = new Regex( @"^(?<type>[U]):(?<universe>[0-5]):(?<id>[0-9]+)$" );
-
-        public static explicit operator ulong( SteamId steamId )
-        {
-            return steamId.Value;
-        }
-
-        public static SteamId Parse( string steamId )
-        {
-            uint id;
-            int instance;
-            byte type;
-            byte universe;
-
-            Match match;
-            if ( (match = _sOldFormatRegex.Match( steamId )).Success )
+            get
             {
-                id = (uint.Parse( match.Groups["id"].Value ) << 1) | uint.Parse( match.Groups["bit"].Value );
-                instance = 1;
-                type = 1;
-                universe = byte.Parse( match.Groups["universe"].Value );
+                return Epoch.AddSeconds( StartTimeSeconds64 / (double) ((ulong) 1 << 31) );
             }
-            else if ( (match = _sNewFormatRegex.Match( steamId )).Success )
+            set
             {
-                id = byte.Parse( match.Groups["id"].Value );
-                instance = 1;
-                type = 1;
-                universe = byte.Parse( match.Groups["universe"].Value );
-            }
-            else
-            {
-                throw new Exception( "Invalid SteamID format." );
-            }
-
-            return new SteamId( id, instance, type, universe );
-        }
-
-        [JsonProperty( "SteamID64" )]
-        public readonly ulong Value;
-
-        [JsonProperty( "SteamID" )]
-        public string V1 => ToString( SteamIdFormat.V1 );
-
-        [JsonProperty( "SteamID3" )]
-        public string V3 => ToString( SteamIdFormat.V3 );
-
-        public SteamId( uint id, int instance, byte type, byte universe )
-        {
-            Value = id | ((ulong) instance << 32) | ((ulong) type << 52) | ((ulong) universe << 56);
-        }
-
-        public SteamId( ulong value )
-        {
-            Value = value;
-        }
-
-        private string ToString( SteamIdFormat format )
-        {
-            var id = Value & 0xffffffff;
-            var instance = (Value >> 32) & 0xfffff;
-            var type = (Value >> 52) & 0xf;
-            var universe = (Value >> 56) & 0xff;
-
-            switch ( format )
-            {
-                case SteamIdFormat.V1:
-                    return $"STEAM_{universe}:{id & 1}:{id >> 1}";
-                case SteamIdFormat.V3:
-                    return $"U:{universe}:{id}";
-                default:
-                    throw new ArgumentException();
+                var diff = (value - Epoch).TotalSeconds;
+                StartTimeSeconds = (uint) diff;
+                StartTimeSeconds64 = (ulong) diff * ((ulong) 1 << 31);
             }
         }
 
-        public override string ToString()
+        [ProtoMember( 3 )]
+        public MatchMessage Match { get; set; } = new MatchMessage();
+
+        [ProtoMember( 4 ), JsonIgnore]
+        public RoundMessage LastRound
         {
-            return ToString( SteamIdFormat.V1 );
+            get { return Rounds.LastOrDefault(); }
+            set { Rounds.Add( value ); }
         }
+
+        [ProtoMember( 5 )]
+        public List<RoundMessage> Rounds { get; set; } = new List<RoundMessage>();
     }
 
-    enum DataType : byte
+    [ProtoContract]
+    public class MatchMessage
     {
-        SteamIdOrTimestamp = 0x08,
-        Unknown10 = 0x10,
-        RoundHeaderLength = 0x12,
-        DemoUrl = 0x1a,
-        RoundDataLength = 0x2a,
-        Kills = 0x28,
-        Assists = 0x30,
-        Deaths = 0x38,
-        Score = 0x40,
-        Winner = 0x58,
-        TeamScore = 0x60,
-        GameTime = 0x78,
-        EnemyKills = 0x80,
-        Headshots = 0x88,
-        Mvps = 0xA8
+        [ProtoMember( 1 )]
+        public int Server { get; set; }
+
+        [ProtoMember( 2 )]
+        public uint Id { get; set; }
+
+        [ProtoMember( 3 )]
+        public int Unknown { get; set; }
+
+        [ProtoMember( 7 )]
+        public ulong Hash { get; set; }
     }
 
-    enum Map : long
+    [ProtoContract]
+    public class RoundMessage
+    {
+        [ProtoMember( 1 )]
+        public ulong? EndTime { get; set; }
+
+        [ProtoMember( 2 )]
+        public RoundHeaderMessage Header { get; set; }
+
+        [ProtoMember( 5 )]
+        public List<int> Kills { get; set; }
+
+        [ProtoMember( 6 )]
+        public List<int> Assists { get; set; }
+
+        [ProtoMember( 7 )]
+        public List<int> Deaths { get; set; }
+
+        [ProtoMember( 8 )]
+        public List<int> Score { get; set; }
+
+        [ProtoMember( 11 )]
+        public int? Winner { get; set; }
+
+        [ProtoMember( 12 )]
+        public List<int> TeamScore { get; set; }
+
+        [ProtoMember( 15 )]
+        public int ElapsedSeconds { get; set; }
+
+        [ProtoMember( 16 )]
+        public List<int> EnemyKills { get; set; }
+
+        [ProtoMember( 17 )]
+        public List<int> Headshots { get; set; }
+
+        [ProtoMember( 21 )]
+        public List<int> Mvps { get; set; }
+    }
+
+    [ProtoContract]
+    public class RoundHeaderMessage
+    {
+        [ProtoMember( 1 )]
+        public List<uint> SteamIds { get; set; } = new List<uint>();
+
+        [ProtoMember( 2 )]
+        public Map? Map { get; set; }
+    }
+
+    public enum Map : long
     {
         DeDust2     = 8 | (1 << 9),
+        DeTrain     = 8 | (1 << 10),
+        DeAztec     = 8 | (1 << 11),
         DeInferno   = 8 | (1 << 12),
         DeNuke      = 8 | (1 << 13),
+        DeVertigo   = 8 | (1 << 14),
         DeMirage    = 8 | (1 << 15),
+        CsOffice    = 8 | (1 << 16),
+        CsAssault   = 8 | (1 << 18),
         DeCache     = 8 | (1 << 20),
+        DeSeason    = 8 | (1 << 21),
+        DeLog       = 8 | (1 << 22),
         CsInsertion = 8 | (1 << 24),
+        DeZoo       = 8 | (1 << 25),
+        DeSantorini = 8 | (1 << 26),
         DeOverpass  = 8 | (1 << 28),
+        DeCbble     = 8 | (1 << 29),
         DeCanals    = 8 | (1 << 30)
-    }
-
-    class DemoInfo
-    {
-        private static string ByteArrayToString( byte[] arr )
-        {
-            return string.Join( " ", arr.Select( x => x.ToString( "x2" ) ) );
-        }
-
-        public static DemoInfo FromFile( string path )
-        {
-            var info = new DemoInfo();
-
-            using ( var reader = new BinaryReader( File.Open( path, FileMode.Open, FileAccess.Read, FileShare.Read ) ) )
-            {
-                if ( reader.ReadByte() != (byte) DataType.SteamIdOrTimestamp ) throw new Exception();
-                info.StartTime = ReadTimeStamp( reader );
-                if ( reader.ReadByte() != 0x10 ) throw new Exception();
-                info.StartTime2 = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc ).AddSeconds( ReadVarInt( reader ) );
-                if ( reader.ReadByte() != 0x1a ) throw new Exception();
-                var firstRoundOffset = reader.ReadByte();
-                if ( reader.ReadByte() != 0x08 ) throw new Exception();
-                info.ServerId = ReadVarInt( reader );
-                if ( reader.ReadByte() != 0x10 ) throw new Exception();
-                info.MatchId = ReadVarInt( reader );
-                if ( reader.ReadByte() != 0x18 ) throw new Exception();
-                info.Unknown0 = ReadVarInt( reader );
-                if ( reader.ReadByte() != 0x38 ) throw new Exception();
-                info.Hash = ReadVarInt( reader );
-
-                while ( reader.BaseStream.Position < reader.BaseStream.Length )
-                {
-                    info.ReadRound( reader );
-                }
-            }
-
-            return info;
-        }
-
-        public DateTime StartTime;
-        public DateTime StartTime2;
-        public long ServerId;
-        public long Unknown0;
-        public Map Map;
-        public long MatchId;
-        public long Hash; // Maybe?
-        public DateTime EndTime;
-        public string DemoUrl;
-        public bool WasTie;
-        public readonly List<RoundData> Rounds = new List<RoundData>();
-
-        private static long ReadVarInt( BinaryReader reader )
-        {
-            ulong val = 0;
-            var shift = 0;
-            byte next;
-            do
-            {
-                next = reader.ReadByte();
-                val |= (ulong) (next & 0x7f) << shift;
-                shift += 7;
-            } while ( (next & 0x80) == 0x80 );
-
-            return (long) val;
-        }
-
-        private static long ReadInt( BinaryReader reader, int bytes )
-        {
-            ulong val = 0;
-            for ( var i = 0; i < bytes; ++i )
-            {
-                val <<= 8;
-                val |= reader.ReadByte();
-            }
-            return (long) val;
-        }
-
-        private static DateTime ReadTimeStamp( BinaryReader reader )
-        {
-            return new DateTime( 1970, 1, 1, 0, 0, 0, DateTimeKind.Utc ).AddSeconds( ReadVarInt( reader ) / (double) ((ulong) 1 << 31) );
-        }
-
-        private void ReadRound( BinaryReader reader )
-        {
-            var round = new RoundData();
-            Rounds.Add( round );
-
-            DataType lastType = 0;
-            int index = 0;
-
-            long dataEnd = reader.BaseStream.Length;
-            long headerEnd = 0;
-
-            while ( reader.BaseStream.Position < dataEnd )
-            {
-                var type = (DataType) reader.ReadByte();
-                if ( lastType != type )
-                {
-                    lastType = type;
-                    index = 0;
-                }
-
-                byte count;
-                switch ( type )
-                {
-                    case DataType.RoundDataLength:
-                        var dataLength = ReadVarInt( reader );
-                        dataEnd = reader.BaseStream.Position + dataLength;
-                        break;
-                    case DataType.SteamIdOrTimestamp:
-                        // Not sure about this
-                        if ( headerEnd == 0 )
-                        {
-                            EndTime = ReadTimeStamp( reader );
-                            break;
-                        }
-                        round.Players[index++].Player = new SteamId( (uint) ReadVarInt( reader ), 1, 1, 1 );
-                        break;
-                    case DataType.Unknown10:
-                        // Always 2 bits set?
-                        Map = (Map) ReadVarInt( reader );
-                        break;
-                    case DataType.RoundHeaderLength:
-                        var headerLength = ReadVarInt( reader );
-                        headerEnd = reader.BaseStream.Position + headerLength;
-                        break;
-                    case DataType.DemoUrl:
-                        var urlLength = ReadVarInt( reader );
-                        DemoUrl = Encoding.ASCII.GetString( reader.ReadBytes( (int) urlLength ) );
-                        break;
-                    case DataType.Kills:
-                        round.Players[index++].Kills = (int) ReadVarInt( reader );
-                        break;
-                    case DataType.Assists:
-                        round.Players[index++].Assists = (int) ReadVarInt( reader );
-                        break;
-                    case DataType.Deaths:
-                        round.Players[index++].Deaths = (int) ReadVarInt( reader );
-                        break;
-                    case DataType.Score:
-                        round.Players[index++].Score = (int) ReadVarInt( reader );
-                        break;
-                    case DataType.TeamScore:
-                        round.Teams[index++].Score = (int) ReadVarInt( reader );
-                        break;
-                    case DataType.Winner:
-                        WasTie = reader.ReadByte() == 0;
-                        break;
-                    case DataType.GameTime:
-                        round.GameTime = TimeSpan.FromSeconds( ReadVarInt( reader ) );
-                        break;
-                    case DataType.EnemyKills:
-                        count = reader.ReadByte();
-                        round.Players[index++].EnemyKills = (int) ReadInt( reader, count );
-                        break;
-                    case DataType.Headshots:
-                        count = reader.ReadByte();
-                        round.Players[index++].Headshots = (int) ReadInt( reader, count );
-                        break;
-                    case DataType.Mvps:
-                        count = reader.ReadByte();
-                        round.Players[index++].Mvps = (int) ReadInt( reader, count );
-                        break;
-                    default:
-                        throw new NotImplementedException( ((byte) type).ToString( "x2" ) );
-                }
-            }
-        }
     }
 
     class Program
     {
-        static string ToBin( long value, int bits )
-        {
-            var str = "";
-            for (var i = bits - 1; i >= 0; --i)
-            {
-                str += (value >> i) & 1;
-            }
-            return str;
-        }
-
         static void Main( string[] args )
         {
             var settings = new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore };
 
             //const string defaultDir = @"C:\Users\James\Documents\GitHub\DemoInfoParser\Examples";
-            const string defaultDir = @"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\replays";
+            //const string defaultDir = @"C:\Program Files (x86)\Steam\steamapps\common\Counter-Strike Global Offensive\csgo\replays";
+            var defaultDir = Path.GetDirectoryName( Assembly.GetExecutingAssembly().Location );
 
             var dir = args.Length != 0 ? args[0] : defaultDir;
             foreach ( var demoInfoPath in Directory.GetFiles( dir, "*.info", SearchOption.TopDirectoryOnly ) )
@@ -353,20 +152,23 @@ namespace DemoInfoTest
 
                 try
                 {
-                    var info = DemoInfo.FromFile( demoInfoPath );
-                    Console.WriteLine( $"{name}: {info.Unknown0}, {info.Hash:x16}, {info.Map}" );
-                    var outPath = $"{Path.GetFileNameWithoutExtension( demoInfoPath )}.txt";
-                    File.WriteAllText( outPath, JsonConvert.SerializeObject( info, Formatting.Indented, settings ) );
+                    using ( var stream = File.Open( demoInfoPath, FileMode.Open, FileAccess.Read, FileShare.Read ) )
+                    {
+                        var info = Serializer.Deserialize<DemoInfoMessage>( stream );
+                        Console.WriteLine( $"{name}: {info.Match.Unknown}, {info.Match.Hash:x16}" );
+
+                        var outPath = $"{Path.GetFileNameWithoutExtension( demoInfoPath )}.txt";
+                        File.WriteAllText( outPath, JsonConvert.SerializeObject( info, Formatting.Indented, settings ) );
+                    }
                 }
                 catch ( Exception e )
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine( name );
                     Console.WriteLine( e );
                     Console.ResetColor();
                 }
             }
-
-            Console.ReadKey();
         }
     }
 }
